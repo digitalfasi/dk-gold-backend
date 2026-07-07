1d37cac3466db91bfe279af60f7d941979024f90
digitalfasi <digitalfasi1999@gmail.com>
2026-07-07 09:47:19 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ const path = require('path');
 const axios = require('axios');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // audio serving is set up below, after AUDIO_DIR is defined
